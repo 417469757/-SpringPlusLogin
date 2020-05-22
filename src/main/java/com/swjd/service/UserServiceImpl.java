e241ee7cb50dba51490ec80ef2d18d942fbf2e83
417469757 <417469757@qq.com>
2020-05-22 15:54:06 +0800

    first

---
+++ src/main/java/com/swjd/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.swjd.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.swjd.bean.User;
+import com.swjd.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService{
+
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public User login(User user) {
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("uname",user.getuName()).eq("password",user.getPassword());
+        return userMapper.selectOne(queryWrapper);
+    }
+}
